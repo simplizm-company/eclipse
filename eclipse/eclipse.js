52fbc3dd7551165f1dfca136514c4930468d3eb6
simplizm-company <40747170+simplizm-company@users.noreply.github.com>
2019-03-05 10:42:57 +0900

    aa

--- eclipse/eclipse.js
+++ eclipse/eclipse.js
@@ -8,6 +8,62 @@
 ;(function($){
     'use strict';
 
+    $.event.special.clickstart = {
+        setup: function() {
+            var isTouchSupported = 'ontouchstart' in document;
+            var clickstart = isTouchSupported ? 'touchstart' : 'mousedown';
+            $(this).bind(clickstart + '.clickstart-event', function(event) {
+                event.type = 'clickstart';
+                if (isTouchSupported) {
+                    event.posX = event.originalEvent.touches[0].pageX;
+                    event.posY = event.originalEvent.touches[0].pageY;
+                } else {
+                    event.posX = event.pageX;
+                    event.posY = event.pageY;
+                }
+                ($.event.dispatch||$.event.handle).call(this, event);
+            });
+        },
+        teardown: function() {
+            $(this).unbind('.clickstart-event');
+        }
+    };
+
+    $.event.special.clickmove = {
+        setup: function() {
+            var isTouchSupported = 'ontouchmove' in document;
+            var clickmove = isTouchSupported ? 'touchmove' : 'mousemove';
+            $(this).bind(clickmove + '.clickmove-event', function(event) {
+                event.type = 'clickmove';
+                if (isTouchSupported) {
+                    event.posX = event.originalEvent.touches[0].pageX;
+                    event.posY = event.originalEvent.touches[0].pageY;
+                } else {
+                    event.posX = event.pageX;
+                    event.posY = event.pageY;
+                }
+                ($.event.dispatch||$.event.handle).call(this, event);
+            });
+        },
+        teardown: function() {
+            $(this).unbind('.clickmove-event');
+        }
+    };
+
+    $.event.special.clickend = {
+        setup: function() {
+            var isTouchSupported = 'ontouchend' in document;
+            var clickend = isTouchSupported ? 'touchend' : 'mouseup';
+            $(this).bind(clickend + '.clickend-event', function(event) {
+                event.type = 'clickend';
+                ($.event.dispatch||$.event.handle).call(this, event);
+            });
+        },
+        teardown: function() {
+            $(this).unbind('.clickend-event');
+        }
+    };
+
     var Eclipse = window.Eclipse || {};
 
     Eclipse = (function(){
